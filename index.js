374f6d1b8085bc2326f45ef48c5b11fa143e952f
Alex Kobylinski <oleksii.kobylinskyi@oracle.com>
2017-11-10 14:18:48 +0200

    no express app

--- index.js
+++ index.js
@@ -12,35 +12,73 @@ var app = express();
 const Telegraf = require('telegraf');
 const bot = new Telegraf(process.env.BOT_TOKEN);
 
-bot.catch((err) => {
-    console.log('Ooops', err)
-})
+const port = Number(process.env.PORT || 3001);
+
+
+
+// bot.startWebhook('/secret-path', null, 5000)
+
+// expressApp.use(bot.webhookCallback('/secret-path'))
+// bot.setWebhook('https://server.tld:8443/secret-path')
 
 // TODO move this to some authorization middleware module
-var userTypes = {
-    any: function(types) {
-        return function(req, res, next) {
-            if (req.isAuthenticated()) {
-                let admin = types.indexOf(Number(req.user.typeId)) !== -1;
-                res.json({
-                    _id : req.user._id,
-                    typeId : req.user.typeId,
-                    provider : req.user.provider,
-                    username : req.user.username,
-                    name : req.user.name,
-                    admin
-                });
-            } else {
-                res.status(401).json({ error: Error('unauthorized') });
-            }
+// var userTypes = {
+//     any: function(types) {
+//         return function(req, res, next) {
+//             if (req.isAuthenticated()) {
+//                 let admin = types.indexOf(Number(req.user.typeId)) !== -1;
+//                 res.json({
+//                     _id : req.user._id,
+//                     typeId : req.user.typeId,
+//                     provider : req.user.provider,
+//                     username : req.user.username,
+//                     name : req.user.name,
+//                     admin
+//                 });
+//             } else {
+//                 res.status(401).json({ error: Error('unauthorized') });
+//             }
+//         }
+//     }
+// };
+
+// app.get('/auth/user/me', userTypes.any(AdminUserIdTypes));
+
+
+// app.listen(port ,function() {
+//     console.log('server running at localhost:' + port + ', go refresh and see magic');
+// });
+
+bot.startPolling();
+
+bot.context.db = {
+    getScores: () => { return 42 },
+    getLol: (username) => {
+        if (username !== "kblnsk") {
+            return null;
         }
-    }
+
+        return "алина певитса @ гмейл ком";
+    },
+    getHello: () => { return "Меня Святослав Викторович зовут."; }
 };
 
-app.get('/auth/user/me', userTypes.any(AdminUserIdTypes));
+bot.start((ctx) => {
+    console.log('started:', ctx.from.id);
+    return ctx.reply('ты почему сюда звонишь? я разрешал тебе сюда звонить?');
+})
 
-var port = Number(process.env.PORT || 3001);
+bot.hears(/привет/i, (ctx) => {
+    return ctx.reply(ctx.db.getHello());
+});
 
-app.listen(port ,function() {
-    console.log('server running at localhost:' + port + ', go refresh and see magic');
+bot.on('text', (ctx) => {
+    const score = ctx.db.getLol(ctx.message.from.username)
+    if (score) {
+        return ctx.reply(`${ctx.message.from.username}: ${score}`)
+    }
+});
+
+bot.catch((err) => {
+    console.log('Ooops', err)
 });
